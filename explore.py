922013630544c8496711b2792e6546c84e44146b
Kwame Taylor <maxkwametaylor@gmail.com>
2020-11-20 13:03:07 -0600

    make explore module

---
+++ explore.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# default viz size settings
+plt.rc('figure', figsize=(9, 7))
+plt.rc('font', size=13)
+
+from wordcloud import WordCloud
+
